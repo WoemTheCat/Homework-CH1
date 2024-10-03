378f124c409d543d0061ad79aa635275313769e5
WoemTheCat <t111820031@ntut.org.tw>
2024-10-03 10:20:52 +0800

    Sample2.java

--- Sample1.java
+++ Sample1.java
@@ -1,7 +1,5 @@
-public class Sample1
-{
-    public static void main(String[] args)
-    {
+public class Sample1 {
+    public static void main(String[] args) {
         System.out.print("歡迎使用Java!");
         System.out.print("開始使用Java吧!");
     }
