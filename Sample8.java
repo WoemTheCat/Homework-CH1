1bc8d139dfeefc44ab8c39727794ad58b7d53154
WoemTheCat <t111820031@ntut.org.tw>
2024-10-03 13:42:16 +0800

    update Sample8.java and create Sample9.java

--- Sample8.java
+++ Sample8.java
@@ -5,7 +5,7 @@ public class Sample8 {
         System.out.println("變數num的值是" + num);
 
         num = 5;
-        System.out.println("更新變數num的值" + num);
+        System.out.println("更新變數num的值");
         System.out.println("變數num更新後的值是" + num);
     }
 }
